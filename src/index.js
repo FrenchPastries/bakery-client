8e1e643c59bbe5010c950cbf1fe7dd16b75cd615
arthur <arthur.escriou@gmail.com>
2019-02-08 12:45:45 +0100

    :sparkles: add fetch

--- src/index.js
+++ src/index.js
@@ -1,41 +1,16 @@
-const http = require('http')
+const fetch = require('node-fetch')
 
 const register = (hostname, port, service) => {
 
-  const options = {
-    hostname,
-    port,
-    path: '/register',
-    method: 'POST',
+  return fetch('http://' + hostname + ':' + port + '/register', {
+    method: 'post',
+    body: JSON.stringify(service),
     headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': JSON.stringify(service).length
-    },
-    body: JSON.stringify(service)
-  }
-
-  const req = http.request(options, (res) => {
-    console.log('res', res)
-    console.log(`statusCode: ${res.statusCode}`)
-    let data = ''
-    res.on('data', (d) => {
-      data += d
-    })
-
-    res.on('end', () => {
-      serviceUuid = JSON.parse(data)
-      console.log(serviceUuid)
-    })
-  })
-
-  req.on('error', (error) => {
-    console.error(error)
-  })
-
-  req.write(JSON.stringify(service))
-  req.end()
-
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
 }
-exports.default = {
+
+module.exports = {
   register
 }
