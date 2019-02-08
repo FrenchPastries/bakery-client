68554cd7f478b202c125d99e3b7ed1ebe1ace8a5
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-02-08 17:03:50 +0100

    :sparkles: Add middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
 const fetch = require('node-fetch')
 
-const register = (hostname, port, service) => {
-
-  return fetch('http://' + hostname + ':' + port + '/register', {
+const registerService = ({ hostname, port, api }) => {
+  return fetch(`http://${hostname}:${port}/register`, {
     method: 'post',
-    body: JSON.stringify(service),
+    body: JSON.stringify(api),
     headers: {
       'Content-Type': 'application/json'
     }
-  }).then(res => res.json())
+  }).then((res) => res.json())
+}
+
+const pingResponse = (uuid) => ({
+  statusCode: 200,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(uuid)
+})
+
+const pingOrHandle = (handler, request) => (uuid) => {
+  if (request.url.pathname === '/ping') {
+    return pingResponse(uuid)
+  } else {
+    return handler(request)
+  }
+}
+
+const responseError = (error) => {
+  console.error(error)
+  return {
+    statusCode: 500,
+    body: 'Unable to register to Registry.'
+  }
+}
+
+const responsePingOrHandle = (registering) => (handler) => (request) => {
+  return registering
+    .then(pingOrHandle(handler, request))
+    .catch(responseError)
+}
+
+const register = (options) => {
+  const registering = registerService(options)
+  return responsePingOrHandle(registering)
 }
 
 module.exports = {
