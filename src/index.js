0971e9e52d7a237a8d18a9544c85ac2b2bd3cb85
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-08-25 21:21:09 +0200

    :construction: Begin customer services inference

--- src/index.js
+++ src/index.js
@@ -17,12 +17,79 @@ const pingResponse = uuid => ({
   body: JSON.stringify(uuid),
 })
 
-const generateServiceAPIHelp = value => {
-  return Object.entries(value).reduce((acc, [ verb, paths ]) => {
-    return paths.reduce((acc, path) => {
-      return acc
+const warnForEmptySegment = (input, key, prefix) => {
+  if (!input && process.env.NODE_ENV !== 'production') {
+    console.warn(`[Customer]: You didn’t provide a variable for "${key}" segment path. This would create unexpected behavior on the server. We used "${key}" to fill the empty segment. For information, it’s on the "/${prefix.join('/')}/:${key}" path.`)
+  }
+}
+
+const unifyPaths = restInterface => {
+  return Object.entries(restInterface).reduce((acc, [verb, paths]) => {
+    return paths.reduce((newAcc, path) => {
+      const verbs = newAcc[path] || new Set()
+      return { ...newAcc, [path]: verbs.add(verb) }
     }, acc)
   }, {})
+}
+
+const constructVerb = (verbs, splitted, acc = {}) => {
+  if (splitted.length === 0) {
+    return verbs
+  } else {
+    const segment = splitted[0]
+    const methods = acc[segment] || {}
+    return { ...acc, [segment]: constructVerb(verbs, splitted.slice(1), methods) }
+  }
+}
+
+const groupByPrefix = unifiedPaths => {
+  return Object.entries(unifiedPaths).reduce((acc, [path, verbs]) => {
+    const splitted = path.slice(1).split('/')
+    return constructVerb(verbs, splitted, acc)
+  }, {})
+}
+
+const generateFetchFunction = (method, prefix) => options => {
+  const url = '/' + prefix.join('/')
+  return fetch(url, { ...options, method })
+}
+
+const generateFetchFunctions = (methods, prefix) => {
+  const verbs = [...methods].map(verb => verb.toLowerCase())
+  return verbs.reduce((acc, verb) => {
+    return { ...acc, [verb]: generateFetchFunction(verb, prefix) }
+  }, {})
+}
+
+const generateFetchPath = (fetchPath, input, segment) => {
+  if (segment.startsWith(':')) {
+    const correctSegment = segment.slice(1)
+    warnForEmptySegment(input, correctSegment, fetchPath)
+    return [...fetchPath, input || correctSegment]
+  } else {
+    return [...fetchPath, segment]
+  }
+}
+
+const generateFunctionsInterface = (groupedByPrefix, fetchPath = []) => {
+  return Object.entries(groupedByPrefix).reduce((acc, [segment, methods]) => {
+    if (segment === '') {
+      const functions = generateFetchFunctions(methods, fetchPath)
+      return { ...acc, ...functions }
+    } else {
+      return { ...acc, [segment.replace(/^:/, '')]: input => {
+        const finalFetchPath = generateFetchPath(fetchPath, input, segment)
+        return generateFunctionsInterface(methods, finalFetchPath)
+      } }
+    }
+  }, {})
+}
+
+const generateServiceAPIHelp = restInterface => {
+  const unifiedPaths = unifyPaths(restInterface)
+  const groupedByPrefix = groupByPrefix(unifiedPaths)
+  const finalInterface = generateFunctionsInterface(groupedByPrefix)
+  return finalInterface
 }
 
 const generateServiceAPI = ({ type, value }) => {
@@ -34,11 +101,8 @@ const generateServiceAPI = ({ type, value }) => {
 }
 
 const generateServicesAPI = interfaces => {
-  return Object.entries(interfaces).reduce((acc, [ serviceName, interface_ ]) => {
-    return {
-      ...acc,
-      [serviceName]: generateServiceAPI(interface_),
-    }
+  return Object.entries(interfaces).reduce((acc, [serviceName, interface_]) => {
+    return { ...acc, [serviceName]: generateServiceAPI(interface_) }
   }, {})
 }
 
@@ -47,7 +111,7 @@ const pingOrHandle = (glob, handler, request) => uuid => {
   if (url.pathname === '/heartbeat') {
     if (body !== glob.interfaces) {
       glob.interfaces = body
-      glob.services = glob.interfaces // generateServicesAPI(JSON.parse(glob.interfaces))
+      glob.services = generateServicesAPI(JSON.parse(glob.interfaces))
     }
     return pingResponse(uuid)
   } else {
